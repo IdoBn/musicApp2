42b4a939d3d5028006da7b6c28b38a563fccb408
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-12-06 17:35:40 +0200

    Initial Commit

---
+++ musicApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musicApp2
+//
+//  Created by Ido Ben-Natan on 12/6/14.
+//  Copyright (c) 2014 Ido Ben-Natan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
